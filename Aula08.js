2430865b2ff994eed0df0cd037839a7b07f847e8
Matheus lima <matheuslimadasilva0@gmail.com>
2024-10-22 20:21:05 -0300

    aulajava

--- Aula08.js
+++ Aula08.js
@@ -1,7 +1,13 @@
 //Aqui iremos entender sobre array dentro do javascript
 
-const investimentoMensal = [1000, 2000, 3000, 4000, 5000, 6000];
+const investimentoMensal = [
+    1000, //00
+    2000, //01
+    3000, //2
+    4000, //3
+    5000, //4
+    6000];//5
 
 console.log(investimentoMensal);
 
-console.log(`O valor investido no mes de janeiro foi.....`);
+console.log(`O valor investido no mes de janeiro foi: ${investimentoMensal[5]}`);

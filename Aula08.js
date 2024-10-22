3ba09430a6015c3888b6049a2980470202150651
Matheus lima <matheuslimadasilva0@gmail.com>
2024-10-22 20:24:33 -0300

    aulajava

--- Aula08.js
+++ Aula08.js
@@ -1,5 +1,6 @@
 //Aqui iremos entender sobre array dentro do javascript
 
+// O indice sempre irá começar pelo (0) nunca pelo (1)
 const investimentoMensal = [
     1000, //00
     2000, //01
@@ -11,3 +12,4 @@ const investimentoMensal = [
 console.log(investimentoMensal);
 
 console.log(`O valor investido no mes de janeiro foi: ${investimentoMensal[5]}`);
+//Acesso ao indice do elemento array

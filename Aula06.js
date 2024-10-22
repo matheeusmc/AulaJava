dfce97149c7d5250f885f584db2194d8817424cb
Matheus lima <matheuslimadasilva0@gmail.com>
2024-10-22 19:51:14 -0300

    aulajava

--- Aula06.js
+++ Aula06.js
@@ -11,4 +11,8 @@ const Carta = {
 //console.log(`Meu personagem se chama: ${Carta.nome}`);
 
 console.Carta = `nome:$(nome) minha habilidade: $(habilidade)`;
-console.log(Carta);
+console.log(Carta.nome);
+console.log(Carta.habilidade);
+console.log(Carta.defesa);
+console.log(Carta.ataque);
+console.log(Carta.slogan);
